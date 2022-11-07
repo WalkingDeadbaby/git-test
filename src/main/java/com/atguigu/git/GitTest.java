7a967e7137dc3d25acbc75c113c42829eef97ffc
WJJ <WJJ@qq.com>
2022-11-07 17:41:03 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author walkingdead
+ * @create 2022-11-07 17:30
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
